95a2a4e5b0dc53cb3d7933210552e64dd284a442
Matías M. Méndez <matimendez88@gmail.com>
2015-07-15 16:55:34 -0300

    mocks changes

--- mocks/mock.js
+++ mocks/mock.js
@@ -4,5 +4,19 @@ $.mockjax({
     proxy: "/mocks/responses/items-get.json"
 });
 
+$.mockjax({
+    url: "/items",
+    proxyType: "put",
+    proxy: "/mocks/responses/items-get.json"
+});
+
+$.mockjax({
+    url: "/listings",
+    proxyType: "get",
+    proxy: "/mocks/responses/listings-get.json"
+});
+
+
+
 // Disable Mocks
 //$.mockjax.clear();
