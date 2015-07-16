5991d53f0823f56462fbed0fdf58abbd6589ae13
Matías M. Méndez <matimendez88@gmail.com>
2015-07-16 11:55:38 -0300

    js templates

--- app/templates/templates.js
+++ app/templates/templates.js
@@ -1,24 +1,58 @@
 this["__templates"] = this["__templates"] || {};
-this["__templates"]["item"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
-    return "";
+this["__templates"]["actions"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+    return "<div class=\"actions-container\">\n    <input type=\"submit\" name=\"\" value=\"Modificar todos\" class=\"ch-btn ch-btn-skin\" data-js=\"modifyAll\">\n    <input type=\"submit\" name=\"\" value=\"Republicar todos\" class=\"ch-btn ch-btn-skin\" data-js=\"resellAll\">\n    <div class=\"filters\">\n    	<p>Filtrar por tipo de listing: </p>\n    	<select>\n    		<option value=\"all\">Todos</option>\n			<option value=\"free\">Gratuita</option>\n			<option value=\"bronze\">Bronce</option>\n			<option value=\"silver\">Plata</option>\n			<option value=\"gold\">Oro</option>\n			<option value=\"goldPro\">Oro Premium</option>\n		</select>\n    </div>\n</div>\n<ul class=\"grid\"></ul>";
 },"useData":true});
-this["__templates"]["itemBack"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
-    var helper, alias1=helpers.helperMissing, alias2="function", alias3=this.escapeExpression;
+this["__templates"]["itemBack"] = Handlebars.template({"1":function(depth0,helpers,partials,data,blockParams,depths) {
+    var stack1, helper, alias1=helpers.helperMissing, alias2=this.lambda, alias3=this.escapeExpression, alias4="function";
+
+  return "                <li class=\"listing-type "
+    + ((stack1 = (helpers.compare || (depth0 && depth0.compare) || alias1).call(depth0,(depth0 != null ? depth0.listingId : depth0),((stack1 = (depths[1] != null ? depths[1].model : depths[1])) != null ? stack1.listingId : stack1),{"name":"compare","hash":{"operator":"==="},"fn":this.program(2, data, 0, blockParams, depths),"inverse":this.noop,"data":data})) != null ? stack1 : "")
+    + "\">\n                    <input name=\"listing-"
+    + alias3(alias2(((stack1 = (depths[1] != null ? depths[1].model : depths[1])) != null ? stack1.id : stack1), depth0))
+    + "\" id=\""
+    + alias3(((helper = (helper = helpers.listingId || (depth0 != null ? depth0.listingId : depth0)) != null ? helper : alias1),(typeof helper === alias4 ? helper.call(depth0,{"name":"listingId","hash":{},"data":data}) : helper)))
+    + "-"
+    + alias3(alias2(((stack1 = (depths[1] != null ? depths[1].model : depths[1])) != null ? stack1.id : stack1), depth0))
+    + "\" type=\"radio\" "
+    + ((stack1 = (helpers.compare || (depth0 && depth0.compare) || alias1).call(depth0,(depth0 != null ? depth0.listingId : depth0),((stack1 = (depths[1] != null ? depths[1].model : depths[1])) != null ? stack1.listingId : stack1),{"name":"compare","hash":{"operator":"==="},"fn":this.program(4, data, 0, blockParams, depths),"inverse":this.noop,"data":data})) != null ? stack1 : "")
+    + " value=\""
+    + alias3(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : alias1),(typeof helper === alias4 ? helper.call(depth0,{"name":"name","hash":{},"data":data}) : helper)))
+    + "\">\n                    <label for=\""
+    + alias3(((helper = (helper = helpers.listingId || (depth0 != null ? depth0.listingId : depth0)) != null ? helper : alias1),(typeof helper === alias4 ? helper.call(depth0,{"name":"listingId","hash":{},"data":data}) : helper)))
+    + "-"
+    + alias3(alias2(((stack1 = (depths[1] != null ? depths[1].model : depths[1])) != null ? stack1.id : stack1), depth0))
+    + "\">\n                        <span>"
+    + alias3(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : alias1),(typeof helper === alias4 ? helper.call(depth0,{"name":"name","hash":{},"data":data}) : helper)))
+    + "</span>\n                    </label>\n                    <label for=\""
+    + alias3(((helper = (helper = helpers.listingId || (depth0 != null ? depth0.listingId : depth0)) != null ? helper : alias1),(typeof helper === alias4 ? helper.call(depth0,{"name":"listingId","hash":{},"data":data}) : helper)))
+    + "-"
+    + alias3(alias2(((stack1 = (depths[1] != null ? depths[1].model : depths[1])) != null ? stack1.id : stack1), depth0))
+    + "\">\n                        <strong>"
+    + alias3((helpers.math || (depth0 && depth0.math) || alias1).call(depth0,(depth0 != null ? depth0.id : depth0),"*",3,{"name":"math","hash":{},"data":data}))
+    + " veces</strong> más visitas\n                    </label>\n                </li>\n";
+},"2":function(depth0,helpers,partials,data) {
+    return "listing-selected";
+},"4":function(depth0,helpers,partials,data) {
+    return "checked";
+},"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data,blockParams,depths) {
+    var stack1, alias1=this.lambda, alias2=this.escapeExpression;
 
   return "<a href=\"#\" class=\"item-image-container\">\n    <img src=\""
-    + alias3(((helper = (helper = helpers.picture || (depth0 != null ? depth0.picture : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"picture","hash":{},"data":data}) : helper)))
+    + alias2(alias1(((stack1 = (depth0 != null ? depth0.model : depth0)) != null ? stack1.picture : stack1), depth0))
     + "\" alt=\"\" width=\"100\" height=\"100\" />\n</a>\n<div class=\"item-details\">\n    <div class=\"item-title-container\">\n        <input type=\"text\" value=\""
-    + alias3(((helper = (helper = helpers.title || (depth0 != null ? depth0.title : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"title","hash":{},"data":data}) : helper)))
+    + alias2(alias1(((stack1 = (depth0 != null ? depth0.model : depth0)) != null ? stack1.title : stack1), depth0))
     + "\" maxlength=\"60\" size=\"30\" />\n    </div>\n    <div class=\"item-price-container\">\n        <label for=\"price\">Precio: $</label>\n        <input type=\"text\" value=\""
-    + alias3(((helper = (helper = helpers.price || (depth0 != null ? depth0.price : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"price","hash":{},"data":data}) : helper)))
+    + alias2(alias1(((stack1 = (depth0 != null ? depth0.model : depth0)) != null ? stack1.price : stack1), depth0))
     + "\" id=\"price\" maxlength=\"10\" />\n    </div>\n    <div class=\"item-quantity-container\">\n        <label for=\"quantity\">Cantidad: </label>\n        <input type=\"text\" value=\""
-    + alias3(((helper = (helper = helpers.quantity || (depth0 != null ? depth0.quantity : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"quantity","hash":{},"data":data}) : helper)))
+    + alias2(alias1(((stack1 = (depth0 != null ? depth0.model : depth0)) != null ? stack1.quantity : stack1), depth0))
     + "\" id=\"quantity\" maxlength=\"4\" size=\"4\" />\n    </div>\n    <div class=\"item-dragged-container\">\n        <label>Acumula: </label>\n        <div class=\"item-dragged-pill\">\n            <span>\n                Visitas\n                <i class=\"syi-icon-user\"></i>\n                <strong class=\"dragged-visits\">"
-    + alias3(((helper = (helper = helpers.visits || (depth0 != null ? depth0.visits : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"visits","hash":{},"data":data}) : helper)))
+    + alias2(alias1(((stack1 = (depth0 != null ? depth0.model : depth0)) != null ? stack1.visits : stack1), depth0))
     + "</strong>\n            </span>\n            <span>\n                Ventas\n                <i class=\"syi-icon-basket\"></i>\n                <strong class=\"dragged-sales\">"
-    + alias3(((helper = (helper = helpers.sells || (depth0 != null ? depth0.sells : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"sells","hash":{},"data":data}) : helper)))
-    + "</strong>\n            </span>\n        </div>\n    </div>\n    <div class=\"item-listing-container\">\n        <h3>Visitas estimadas para esta categoría *</h3>\n        <ul>\n            <li class=\"listing-type\">\n                <input name=\"listing\" id=\"goldPro\" value=\"\" type=\"radio\">\n                <label for=\"goldPro\" class=\"\">\n                    <span class=\"\">Oro Premium</span>\n                </label>\n                <label for=\"goldPro\" class=\"\">\n                    <strong class=\"\">9 veces</strong> más visitas\n                    <i class=\"ch-icon-help-sign\"></i>\n                </label>\n            </li>\n            <li class=\"listing-type\">\n                <input name=\"listing\" id=\"gold\" value=\"\" type=\"radio\">\n                <label for=\"gold\" class=\"\">\n                    <span class=\"\">Oro</span>\n                </label>\n                <label for=\"gold\" class=\"\">\n                    <strong class=\"\">7 veces</strong> más visitas\n                    <i class=\"ch-icon-help-sign\"></i>\n                </label>\n            </li>\n            <li class=\"listing-type\">\n                <input name=\"listing\" id=\"silver\" value=\"\" type=\"radio\">\n                <label for=\"silver\" class=\"\">\n                    <span class=\"\">Plata</span>\n                </label>\n                <label for=\"silver\" class=\"\">\n                    <strong class=\"\">5 veces</strong> más visitas\n                    <i class=\"ch-icon-help-sign\"></i>\n                </label>\n            </li>\n            <li class=\"listing-type\">\n                <input name=\"listing\" id=\"bronze\" value=\"\" type=\"radio\">\n                <label for=\"bronze\" class=\"\">\n                    <span class=\"\">Bronce</span>\n                </label>\n                <label for=\"bronze\" class=\"\">\n                    <strong class=\"\">Actual</strong>\n                    <i class=\"ch-icon-help-sign\"></i>\n                </label>\n            </li>\n            <li class=\"listing-type\">\n                <input name=\"listing\" id=\"free\" value=\"\" type=\"radio\">\n                <label for=\"free\" class=\"\">\n                    <span class=\"\">Gratuita</span>\n                </label>\n                <label for=\"free\" class=\"\">\n                    1 vez menos visitas\n                    <i class=\"ch-icon-help-sign\"></i>\n                </label>\n            </li>\n        </ul>\n    </div>\n    <div class=\"item-ammounts-container\">\n        <h4>Costo por republicar</h4>\n        <div class=\"price-resell\">\n            <span class=\"ch-price\">$ 0<sup>00</sup></span>\n        </div>\n        <h4 class=\"\">Costo por cada venta</h4>\n        <div class=\"price-per-sell\" data-js=\"\">\n            <span class=\"ch-price\">$ 88<sup>00</sup></span>\n        </div>\n    </div>\n</div>\n<div class=\"item-action-container\">\n    <input type=\"submit\" name=\"\" value=\"Republicar\" class=\"ch-btn ch-btn-large\" data-js=\"resellBtn\">\n</div>";
-},"useData":true});
+    + alias2(alias1(((stack1 = (depth0 != null ? depth0.model : depth0)) != null ? stack1.sells : stack1), depth0))
+    + "</strong>\n            </span>\n        </div>\n    </div>\n    <div class=\"item-listing-container\">\n        <h3>Visitas estimadas para esta categoría *</h3>\n        <ul>\n"
+    + ((stack1 = helpers.each.call(depth0,(depth0 != null ? depth0.listings : depth0),{"name":"each","hash":{},"fn":this.program(1, data, 0, blockParams, depths),"inverse":this.noop,"data":data})) != null ? stack1 : "")
+    + "        </ul>\n    </div>\n    <div class=\"item-ammounts-container\">\n        <div class=\"price-resell-container media-object-fit\">\n            <h4>Costo por republicar</h4>\n            <div class=\"price-resell\">\n                <span class=\"ch-price\">$ 0<sup>00</sup></span>\n            </div>\n        </div>\n        <div class=\"price-per-sell-container media-object-fit-right\">\n            <h4 class=\"\">Costo por cada venta</h4>\n            <div class=\"price-per-sell\" data-js=\"\">\n                <span class=\"ch-price\">$ 88<sup>00</sup></span>\n            </div>\n        </div>\n    </div>\n</div>\n<div class=\"item-action-container\">\n    <input type=\"submit\" name=\"\" value=\"Republicar\" class=\"ch-btn ch-btn-large\" data-js=\"resellBtn\">\n</div>";
+},"useData":true,"useDepths":true});
 this["__templates"]["itemFront"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
     var helper, alias1=helpers.helperMissing, alias2="function", alias3=this.escapeExpression;
 
